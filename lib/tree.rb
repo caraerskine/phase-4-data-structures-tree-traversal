123a131793f157347e9191ea23aa22aba36267b8
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 10:44:47 -0400

    commit tree 5dbba323582f44b1f5a75073c17f20ca5e6f90c1

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,27 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
-end
+
+def get_element_by_id(id)
+    # Define a helper method for depth-first traversal
+    def dfs(node, target_id)
+      return node if node[:id] == target_id
+
+      node[:children].each do |child|
+        result = dfs(child, target_id)
+        return result if result
+      end
+
+      nil
+    end
+
+    # Start the traversal from the root node
+    dfs(@root, id)
+  end
+
+
+end 
+
+
+
+
